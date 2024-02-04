d10f874f2182780df4296fee84dc81c239a725e4
Tajul <subqitajul7@gmail.com>
2024-02-04 14:11:49 +0700

    solve img

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -58,7 +58,7 @@ const HeroSection = () => {
         >
           <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <img
-              src="./images/myprofile.jpg"
+              src="./images/myprofile.png"
               alt="hero image"
               className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full aspect-square object-cover"
               width={400}
